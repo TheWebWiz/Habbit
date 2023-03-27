c2c95e7c63885f05678fd59e847c14716c1a5272
James <thewebwizard@outlook.com>
2023-03-27 12:49:10 -0400

    created header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Header.css';
+
+function Header(){
+    return (
+        <header>
+            <h1>Habbit</h1>
+            <input type="text" />
+            <img src="#" alt="Search Icon" />
+        </header>
+    )
+}
+export default Header;
