265efd8f58b79faf22a6b2f383c7ff35a905e1a7
James <thewebwizard@outlook.com>
2023-03-29 14:41:46 -0400

    Api call and action function in store

--- src/components/assets/PostList/PostList.jsx
+++ src/components/assets/PostList/PostList.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Post from "../Post/Post";
 import './PostList.css';
 import { useDispatch, useSelector } from "react-redux";
+import { 
+    fetchPosts,
+    selectFilteredPosts,
+    setSearchTerm,
+    fetchComments,
+} from "../../../store/redditSlice";
 
 function PostList() {
+    const reddit = useSelector((state) => state.reddit);
+    const { isLoading, error, searchTerm, selectedSubreddit } = reddit;
+    const posts = useSelector(selectFilteredPosts);
     const dispatch = useDispatch();
-    const { posts } = useSelector((store) => store.reddit);
+
+    useEffect(() => {
+        dispatch(fetchPosts(selectedSubreddit));
+    }, [selectedSubreddit]);
 
     return (
         <section>
-           {posts.map((post) => {
-            return <Post key={post.id} {...item} />;
-           })}
+           
         </section>
     )
 }
