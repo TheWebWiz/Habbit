daa2ebbe042484adaf511a6980d83a4cefef4044
James <thewebwizard@outlook.com>
2023-03-30 11:12:20 -0400

    added change subreddit!!

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './Aside.css';
 import Subreddit from "../assets/subreddit/subreddit";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectSelectedSubreddit, setSelectedSubreddit } from "../../store/redditSlice";
 
 function Aside() {
-    const {subreddits} = useSelector((state) => state.subreddit)
+    const {subreddits} = useSelector((state) => state.subreddit);
+
 
     return (
         <aside>
@@ -17,6 +19,7 @@ function Aside() {
                         id={subreddit.id}
                         name={subreddit.name}
                         img={subreddit.img}
+                        url={subreddit.link}
                     />
                    )
                 })}
--- src/components/assets/Subreddit/Subreddit.jsx
+++ src/components/assets/Subreddit/Subreddit.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectSelectedSubreddit, setSelectedSubreddit } from "../../../store/redditSlice.js";
 import { EmptyReddit } from "../utils/svg.jsx";
 import './Subreddit.css';
 
-function Subreddit({ id, name, img }) {
+function Subreddit({ id, name, img, url }) {
+    const dispatch = useDispatch();
+    const selectedSubreddit = useSelector(selectSelectedSubreddit)
+
     let subredditImg = img === '' ? <EmptyReddit /> : <img src={img} alt={id} />;
+    
     return (
-        <button>
+        <button
+            onClick={() => dispatch(setSelectedSubreddit(url))}
+        >
             <div>
                 {subredditImg}
             </div>
